6e5870a84a764ad1b6e38ee9c8913f1f4b84efa1
Dylan Foster <dylantf@gmail.com>
2025-09-20 16:30:39 +0200

    Add parser for 2013 CSV

--- app/Main.hs
+++ app/Main.hs
@@ -8,5 +8,7 @@ fullPath filename = "/home/dylan/Desktop/Sesh Import/" ++ filename
 
 main :: IO ()
 main = do
-  contents2012 <- Schemas.parse2012 (fullPath "2012.csv")
-  forM_ contents2012 $ \row -> print row
+  -- contents2012 <- Schemas.parse2012 (fullPath "2012.csv")
+  contents2013 <- Schemas.parse2013 (fullPath "2013.csv")
+  -- forM_ contents2012 $ \row -> print row
+  forM_ contents2013 $ \row -> print row
--- app/Schemas.hs
+++ app/Schemas.hs
@@ -1,4 +1,4 @@
-module Schemas (Normalized, parse2012) where
+module Schemas (Normalized, parse2012, parse2013) where
 
 import Data.ByteString.Lazy qualified as BL
 import Data.Csv
@@ -15,6 +15,12 @@ data Sport
   | Parawinging
   deriving (Show)
 
+data SeshType
+  = Spot
+  | Downwinder
+  | Roundwinder
+  deriving (Show)
+
 data Normalized = Normalized
   { date :: Day,
     sport :: Sport,
@@ -23,65 +29,10 @@ data Normalized = Normalized
     windGust :: Maybe Int,
     kiteSize :: Maybe String,
     wingSize :: Maybe String,
-    seshType :: String,
+    seshType :: Maybe SeshType,
     comments :: String
   }
   deriving (Show)
-
-normalizeSport :: String -> Sport
-normalizeSport s = case s of
-  "Kiteboarding" -> Kiteboarding
-  "SUP" -> SUP
-  "Skiing" -> Skiing
-  "Snowboarding" -> Snowboarding
-  "Surfing" -> Surfing
-  "Wing Foiling" -> WingFoiling
-  "Parawinging" -> Parawinging
-  _ -> error $ "Unknown sport: " ++ s
-
-parseDate :: String -> Parser Day
-parseDate dateStr =
-  case parseTimeM True defaultTimeLocale "%-m/%-d/%Y" dateStr of
-    Just day -> pure day
-    Nothing -> error $ "Could not parse date: " ++ dateStr
-
-data CsvRow2012 = CsvRow2012
-  { date_2012 :: Day,
-    sport_2012 :: String,
-    hours_2012 :: Double,
-    windAvg_2012 :: Int,
-    windGust_2012 :: Int,
-    kiteSize_2012 :: String,
-    seshType_2012 :: String,
-    comments_2012 :: String
-  }
-  deriving (Show)
-
-instance FromNamedRecord CsvRow2012 where
-  parseNamedRecord r =
-    CsvRow2012
-      <$> (r .: "Date" >>= parseDate)
-      <*> r .: "Sport"
-      <*> r .: "Hours"
-      <*> r .: "Lull (kn)"
-      <*> r .: "Gust (kn)"
-      <*> r .: "Kite Size"
-      <*> r .: "Type"
-      <*> r .: "Comments"
-
-normalize2012 :: CsvRow2012 -> Normalized
-normalize2012 row =
-  Normalized
-    { date = date_2012 row,
-      sport = normalizeSport $ sport_2012 row,
-      hours = hours_2012 row,
-      windAvg = Just (windAvg_2012 row),
-      windGust = Just (windGust_2012 row),
-      kiteSize = Just (kiteSize_2012 row),
-      wingSize = Nothing,
-      seshType = seshType_2012 row,
-      comments = comments_2012 row
-    }
 
 readCsvFile :: (FromNamedRecord a) => String -> IO [a]
 readCsvFile path = do
@@ -95,5 +46,115 @@ parseCsvFile path normalizeFn = do
   contents <- readCsvFile path
   pure $ normalizeFn <$> contents
 
+normalizeSport :: String -> Sport
+normalizeSport s = case s of
+  "Kiteboarding" -> Kiteboarding
+  "SUP" -> SUP
+  "Skiing" -> Skiing
+  "Snowboarding" -> Snowboarding
+  "Surfing" -> Surfing
+  "Surf" -> Surfing
+  "Wing Foiling" -> WingFoiling
+  "Parawinging" -> Parawinging
+  _ -> error $ "Unknown sport: " ++ s
+
+normalizeSeshType :: Maybe String -> Maybe SeshType
+normalizeSeshType s = case s of
+  Nothing -> Nothing
+  Just "Spot" -> Just Spot
+  Just "Downwinder" -> Just Downwinder
+  Just "Roundwinder" -> Just Roundwinder
+  Just other -> error $ "Unhandled session type: " ++ other
+
+parseDate :: String -> Day
+parseDate dateStr =
+  case parseTimeM True defaultTimeLocale "%-m/%-d/%Y" dateStr of
+    Just day -> day
+    Nothing -> error $ "Could not parse date: " ++ dateStr
+
+-- 2012
+
+data CsvRow2012 = CsvRow2012
+  { date_2012 :: String,
+    sport_2012 :: String,
+    hours_2012 :: Double,
+    windAvg_2012 :: Int,
+    windGust_2012 :: Int,
+    kiteSize_2012 :: String,
+    seshType_2012 :: String,
+    comments_2012 :: String
+  }
+  deriving (Show)
+
+instance FromNamedRecord CsvRow2012 where
+  parseNamedRecord r =
+    CsvRow2012
+      <$> r .: "Date"
+      <*> r .: "Sport"
+      <*> r .: "Hours"
+      <*> r .: "Lull (kn)"
+      <*> r .: "Gust (kn)"
+      <*> r .: "Kite Size"
+      <*> r .: "Type"
+      <*> r .: "Comments"
+
+normalize2012 :: CsvRow2012 -> Normalized
+normalize2012 row =
+  Normalized
+    { date = parseDate $ date_2012 row,
+      sport = normalizeSport $ sport_2012 row,
+      hours = hours_2012 row,
+      windAvg = Just (windAvg_2012 row),
+      windGust = Just (windGust_2012 row),
+      kiteSize = Just (kiteSize_2012 row),
+      wingSize = Nothing,
+      seshType = normalizeSeshType $ Just $ seshType_2012 row,
+      comments = comments_2012 row
+    }
+
 parse2012 :: String -> IO [Normalized]
 parse2012 path = parseCsvFile path normalize2012
+
+-- 2013
+-- CSV Header: Date,Sport,Hours,Lull,Gust,Kite,Type,Comments
+
+data CsvRow2013 = CsvRow2013
+  { date_2013 :: String,
+    sport_2013 :: String,
+    hours_2013 :: Double,
+    windAvg_2013 :: Maybe Int,
+    windGust_2013 :: Maybe Int,
+    kiteSize_2013 :: Maybe String,
+    seshType_2013 :: Maybe String,
+    comments_2013 :: String
+  }
+  deriving (Show)
+
+instance FromNamedRecord CsvRow2013 where
+  parseNamedRecord r =
+    CsvRow2013
+      <$> r .: "Date"
+      <*> r .: "Sport"
+      <*> r .: "Hours"
+      <*> r .: "Lull"
+      <*> r .: "Gust"
+      <*> r .: "Kite"
+      <*> r .: "Type"
+      <*> r .: "Comments"
+
+normalize2013 :: CsvRow2013 -> Normalized
+normalize2013 row =
+  Normalized
+    { date = parseDate $ date_2013 row,
+      sport = normalizeSport $ sport_2013 row,
+      hours = hours_2013 row,
+      windAvg = windAvg_2013 row,
+      windGust = windGust_2013 row,
+      kiteSize = kiteSize_2013 row,
+      wingSize = Nothing,
+      seshType = normalizeSeshType $ seshType_2013 row,
+      comments = comments_2013 row
+    }
+
+parse2013 :: String -> IO [Normalized]
+parse2013 path = parseCsvFile path normalize2013
