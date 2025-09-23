98d79ad48de318af94930e50d04579ad92b172ee
Dylan Foster <dylantf@gmail.com>
2025-09-23 20:09:44 +0200

    Add parawinging support to hs app

--- haskell/app/Gear.hs
+++ haskell/app/Gear.hs
@@ -33,24 +33,24 @@ replace from with = go
       | otherwise = x : go xs
 
 kiteId :: (Day, String) -> Maybe Int
-kiteId (seshDate, kiteSize) =
+kiteId (d, kiteSize) =
   let normalizedSize = replace "m" "" kiteSize
-   in case (seshDate, normalizedSize) of
-        (d, "12") | before "2013-01-15" d -> Just 1
-        (d, "8") | before "2013-01-15" d -> Just 2
-        (d, "12") | between ("2013-01-16", "2013-12-07") d -> Just 3
-        (d, "9") | between ("2013-01-16", "2013-12-07") d -> Just 4
-        (d, "13") | between ("2013-12-08", "2014-12-31") d -> Just 6
-        (d, "9") | between ("2013-12-08", "2014-12-31") d -> Just 7
-        (d, "7") | between ("2013-12-08", "2014-12-31") d -> Just 8
-        (d, "12") | between ("2015-01-01", "2017-02-05") d -> Just 9
-        (d, "10") | between ("2016-10-09", "2018-01-24") d -> Just 10
-        (d, "7") | between ("2016-10-09", "2019-10-04") d -> Just 11
-        (d, "12") | between ("2017-02-06", "2022-08-10") d -> Just 12
-        (d, "10") | between ("2018-01-25", "2020-12-31") d -> Just 13
-        (d, "7") | between ("2019-10-05", "2020-12-31") d -> Just 14
-        (d, "9") | after "2021-01-01" d -> Just 15
-        (d, "7") | after "2021-01-01" d -> Just 16
+   in case normalizedSize of
+        "12" | before "2013-01-15" d -> Just 1
+        "8" | before "2013-01-15" d -> Just 2
+        "12" | between ("2013-01-16", "2013-12-07") d -> Just 3
+        "9" | between ("2013-01-16", "2013-12-07") d -> Just 4
+        "13" | between ("2013-12-08", "2014-12-31") d -> Just 6
+        "9" | between ("2013-12-08", "2014-12-31") d -> Just 7
+        "7" | between ("2013-12-08", "2014-12-31") d -> Just 8
+        "12" | between ("2015-01-01", "2017-02-05") d -> Just 9
+        "10" | between ("2016-10-09", "2018-01-24") d -> Just 10
+        "7" | between ("2016-10-09", "2019-10-04") d -> Just 11
+        "12" | between ("2017-02-06", "2022-08-10") d -> Just 12
+        "10" | between ("2018-01-25", "2020-12-31") d -> Just 13
+        "7" | between ("2019-10-05", "2020-12-31") d -> Just 14
+        "9" | after "2021-01-01" d -> Just 15
+        "7" | after "2021-01-01" d -> Just 16
         _ -> Nothing
 
 kiteIds :: Normalized -> [Int]
@@ -62,20 +62,20 @@ kiteIds row =
     _ -> []
 
 hydrofoilId :: (Day, Maybe String) -> Maybe Int
-hydrofoilId (day, foilName) = case (day, foilName) of
-  (d, Nothing) | before "2019-05-26" d -> Just 17
-  (d, Nothing) | after "2019-05-27" d -> Just 18
-  (_, Just "Thruster") -> Just 18
-  (_, Just "ART 999") -> Just 19
-  (_, Just "ART 799") -> Just 20
-  (_, Just "Phantom 1480") -> Just 21
-  (_, Just "Seven Seas 1200") -> Just 22
-  (_, Just "Phantom-S 840") -> Just 23
-  (_, Just "Eagle 890") -> Just 24
-  (_, Just "Eagle 990") -> Just 25
-  (_, Just "Ypra-S 785") -> Just 26
-  (_, Just "Ypra-S 1000") -> Just 27
-  (_, Just "Veloce 890") -> Just 28
+hydrofoilId (d, foilName) = case foilName of
+  Nothing | before "2019-05-26" d -> Just 17
+  Nothing | after "2019-05-27" d -> Just 18
+  Just "Thruster" -> Just 18
+  Just "ART 999" -> Just 19
+  Just "ART 799" -> Just 20
+  Just "Phantom 1480" -> Just 21
+  Just "Seven Seas 1200" -> Just 22
+  Just "Phantom-S 840" -> Just 23
+  Just "Eagle 890" -> Just 24
+  Just "Eagle 990" -> Just 25
+  Just "Ypra-S 785" -> Just 26
+  Just "Ypra-S 1000" -> Just 27
+  Just "Veloce 890" -> Just 28
   _ -> Nothing
 
 usesFoil :: Normalized -> Bool
@@ -90,16 +90,16 @@ hydrofoilIds row =
     (True, d, f) -> maybeToList $ hydrofoilId (d, f)
 
 boardId :: (Day, Maybe String) -> Maybe Int
-boardId (day, boardName) = case (day, boardName) of
-  (_, Just "Groove Skate") -> Just 31
-  (d, Nothing) | after "2022-08-10" d -> Just 31
-  (_, Just "Rocket v2 85L") -> Just 32
-  (_, Just "Rocket v2 60L") -> Just 34
-  (_, Just "Rocket 60L") -> Just 34
-  (_, Just "Flying Fish 40L") -> Just 33
-  (_, Just "LF Galaxy") -> Just 30
-  (d, Nothing) | between ("2017-06-24", "2022-08-09") d -> Just 30
-  (d, Nothing) | before "2017-06-23" d -> Just 17
+boardId (d, boardName) = case boardName of
+  Just "Groove Skate" -> Just 31
+  Nothing | after "2022-08-10" d -> Just 31
+  Just "Rocket v2 85L" -> Just 32
+  Just "Rocket v2 60L" -> Just 34
+  Just "Rocket 60L" -> Just 34
+  Just "Flying Fish 40L" -> Just 33
+  Just "LF Galaxy" -> Just 30
+  Nothing | between ("2017-06-24", "2022-08-09") d -> Just 30
+  Nothing | before "2017-06-23" d -> Just 17
   _ -> Nothing
 
 -- I only care about foilboards, throw away others
@@ -108,15 +108,16 @@ boardIds row
   | usesFoil row = maybeToList $ boardId (date row, board row)
   | otherwise = []
 
-wingId :: (Day, String) -> Maybe Int
-wingId (day, size) = case (day, size) of
+wingId :: (Sport, Day, String) -> Maybe Int
+wingId (sport, day, size) = case (sport, size) of
   (_, "6m") -> Just 35
-  (d, "5m")
-    | before "2024-01-01" d -> Just 36
-    | after "2024-01-01" d -> Just 39
-  (d, "4m")
-    | before "2024-01-01" d -> Just 37
-    | after "2024-01-01" d -> Just 40
+  (_, "5m")
+    | before "2024-01-01" day -> Just 36
+    | after "2024-01-01" day -> Just 39
+  (WingFoiling, "4m")
+    | before "2024-01-01" day -> Just 37
+    | after "2024-01-01" day -> Just 40
+  (Parawinging, "4m") -> Just 42
   (_, "5.5m") -> Just 38
   (_, "3m") -> Just 41
   _ -> Nothing
@@ -130,5 +131,5 @@ wingIds row =
    in case (usesWing, wingSize row) of
         (True, Just wingSizes) ->
           let sizes = splitValues wingSizes
-           in mapMaybe (\size -> wingId (date row, size)) sizes
+           in mapMaybe (\size -> wingId (sport row, date row, size)) sizes
         _ -> []
