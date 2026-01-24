faa6b650e7a5b469a68877257a95795311698209
dylantf <dylantf@gmail.com>
2026-01-24 17:28:07 +0100

    Use cross platform home directory and filepath loading

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -2,13 +2,19 @@ module Main where
 
 import Db (insertData)
 import Parsers (parseFile)
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
 
-filePath :: Int -> String
-filePath year = "/home/dylan/Desktop/Sesh Import/" ++ show year ++ ".csv"
+filePath :: Int -> IO FilePath
+filePath year = do
+  home <- getHomeDirectory
+  return $ home </> "Desktop" </> "Sesh Import" </> show year ++ ".csv"
 
 main :: IO ()
 main = do
   csvData <- concat <$> mapM runParser [2012 .. 2026]
   insertData csvData
   where
-    runParser year = parseFile (filePath year) year
+    runParser year = do
+      path <- filePath year
+      parseFile path year
